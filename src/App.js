f0d287cb22d6033790ddd7442655caac0db87c1f
7zhk <me@7zhk.cn>
2020-10-27 14:45:14 +0800

    新增输入要素名称的功能

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Map from './Map';
+import { Button } from 'antd';
+import 'antd/dist/antd.dark.css'
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-        <div className="title">
-          Streets' Entropy Calculator
-          <br/>
-          <i className="authorName">Xuefei & Jingyuan</i>
-        </div>
-      <Map>
-      </Map>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  mapPage = React.createRef();
+
+  handleSubmit = () => {
+    let data = this.mapPage.current.state.draw.getAll()
+    const features = this.mapPage.current.state.features;
+    data.features = features;
+    console.log(features)
+    console.log(data);
+
+    axios.post('http://127.0.0.1:5000/entropy', data)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+  };
+
+  render() {
+    return (
+      <div className="App">
+          <div className="title">
+            Streets' Entropy Calculator
+            <br/>
+            <i className="author-name">Xuefei & Jingyuan</i>
+          </div>
+        <Button className="submit-button" onClick={this.handleSubmit}>Submit</Button>
+        <Map ref={this.mapPage}>
+        </Map>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import mapboxgl from 'mapbox-gl';
-import turf from 'turf'
+//  import turf from 'turf'
 import MapboxDraw from 'mapbox-gl-draw'
 import './Map.css';
-import axios from 'axios';
+//  import axios from 'axios';
+import { Modal } from 'antd';
+import 'antd/dist/antd.dark.css'
 
 class Map extends Component {
     constructor(props) {
@@ -11,22 +13,33 @@ class Map extends Component {
         this.state = {
             map: {},
             draw: {},
-        }            
+            isModal: false,
+            inputName: '',
+            features: [],
+        }
     }
 
-    updateArea = (e)=>{
+    onCreateItem = (e)=>{
+        this.setState({isModal: true})
         var data = this.state.draw.getAll()
-        if (data.features.length > 0) {
+        let features = this.state.features
+        const listLength = data.features.length
+        const addedItem = data.features[listLength-1]
+        features = features.concat(addedItem)
+        this.setState({features: features})
           //var area = turf.area(data)
           //var rounded_area = Math.round(area * 100) / 100
-          alert('添加成功')
-        }
+          //alert('添加成功')
+
+        /*
         const listLength = data.features.length;
         for (let i = 0;i < listLength;i ++){
             data.features[i]['name']=`第${(i+1).toString()}个`;
         }
+        */
         console.log(data.features);
         //  发请求
+        /*
         axios.post('http://localhost:5000/entropy', data.features)
           .then(function (response) {
             console.log(response);
@@ -34,6 +47,22 @@ class Map extends Component {
           .catch(function (error) {
             console.log(error);
           });
+        */
+    }
+
+    handleOk = () => {
+        const listLength = this.state.features.length;
+        const features = this.state.features;
+        features[listLength-1]['name'] = this.state.inputName;
+        this.setState({
+            isModal: false, 
+            inputName: '',
+            features: features,
+        });
+    };
+
+    handleChange = (event) => {
+        this.setState({inputName: event.target.value});
     }
 
     componentDidMount() {
@@ -57,9 +86,9 @@ class Map extends Component {
           })
         
         map.addControl(draw)
-        map.on('draw.create', this.updateArea)
-        map.on('draw.delete', this.updateArea)
-        map.on('draw.update', this.updateArea)
+        map.on('draw.create', this.onCreateItem)
+        //map.on('draw.delete', this.updateArea)
+        //map.on('draw.update', this.updateArea)
 
         this.setState({
             map:map,
@@ -69,8 +98,22 @@ class Map extends Component {
 
     render() {
         return (
+        <div>
+
             <div id="map" className="map">
             </div>
+
+            <Modal
+                title=""
+                visible={this.state.isModal}
+                onOk={this.handleOk}
+                closable={false}
+                cancelButtonProps={{ disabled: true }}
+            >
+                <p>Enter the name of this zone.</p>
+                <input type="text" className="textInput" onChange={this.handleChange}></input>
+            </Modal>
+        </div>
         );
     }
 }
